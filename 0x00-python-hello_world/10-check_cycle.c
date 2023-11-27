92ab3d2a68cd5db2a5bf5602e444d1b1fae82ef7
adams009 <olaadam003@gmail.com>
2023-11-27 11:44:49 -0800

    good

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function to check cycle
+ * @list: list
+ * Return: 1 or 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+
+	if (!list || !list->next)
+	{
+		return (0);
+	}
+
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
