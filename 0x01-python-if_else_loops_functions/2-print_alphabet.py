eaf4f85b9c46febc4b0ee793e5e9698b5a950ad1
adams009 <olaadam003@gmail.com>
2023-11-28 16:25:35 -0800

    lower_case

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,2 @@
+for ascii_lower in range(ord('a'), ord('z') + 1):
+    print(chr(ascii_lower), end='')
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,3 @@
+for ascii_lower in range(ord('a'), ord('z') + 1):
+    if (chr(ascii_lower) not in ["q", "e"]):
+            print(chr(ascii_lower), end="")
