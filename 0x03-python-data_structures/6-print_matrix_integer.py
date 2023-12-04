d0ccbb2063e5f8671a304fce50bdd4f00d64ae85
adams009 <olaadam003@gmail.com>
2023-12-04 04:48:33 -0800

    good

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
     for row in matrix:
         line = " ".join("{}".format(num) for num in row)
         print(line)
