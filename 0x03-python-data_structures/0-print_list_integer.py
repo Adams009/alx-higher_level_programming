4d7376f680ab7f31e9f1c343c2f3ee009089f4e8
adams009 <olaadam003@gmail.com>
2023-12-03 16:02:02 -0800

    god

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    count = 0
-    new_list = []
-    for integer in my_list:
-        count = count + 1
-    for i in range(count):
-        print("{}".format(my_list[i]))
+    #count = 0
+   # for integer in my_list:
+       # count = count + 1
+    for i in my_list:
+        print("{}".format(my_list[i - 1]))
