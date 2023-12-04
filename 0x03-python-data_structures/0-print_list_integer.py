70f935517705c01e38b3769bfb8b5cbff503de08
adams009 <olaadam003@gmail.com>
2023-12-03 16:03:45 -0800

    god

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    #count = 0
-   # for integer in my_list:
-       # count = count + 1
+def print_list_integer(my_list=[])
     for i in my_list:
         print("{}".format(my_list[i - 1]))
