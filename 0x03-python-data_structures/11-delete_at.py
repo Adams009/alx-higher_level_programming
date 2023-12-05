7385d1f7934cc4c5835107637e3391110a28800f
adams009 <olaadam003@gmail.com>
2023-12-05 13:35:07 -0800

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
 
     if 0 <= idx < len(my_list):
-        new_list = my_list[:idx] = my_list[idx + 1:]
+        new_list = [my_list[i] for i in range(len(my_list)) if i != idx]
 
     return new_list
