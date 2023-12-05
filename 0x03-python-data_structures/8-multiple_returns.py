217bb54f38c9cb422d74601ea7a41c0293496070
adams009 <olaadam003@gmail.com>
2023-12-05 11:45:22 -0800

    good

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     count = 0
     if not sentence:
-        return (None)
+        return (0, None)
     for sen in sentence:
         count+= 1
 
-    return (count, sentence[0])
+    return (len(sentence), sentence[0])
