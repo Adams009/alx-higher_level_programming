b36ab647347c6ced0dad88f641f8d62284f64f7b
adams009 <olaadam003@gmail.com>
2023-12-06 16:54:22 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set11 = list(set_1)
-    set22 = list(set_2)
-    count = 0
-    new = ""
-
-    for i in set11:
-        count += 1
-
-    for l in range(count):
-        if set11[l] == set22[l]:
-            new = set11[l]
-
-    return new
+    new_set = set_1.intersection(set_2)
+    return new_set
