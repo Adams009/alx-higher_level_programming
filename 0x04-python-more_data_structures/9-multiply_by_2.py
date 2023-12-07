96b8d6b272ae59099bc4444b56911986c815f09a
adams009 <olaadam003@gmail.com>
2023-12-06 18:28:24 -0800

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    multiplied_dict = a_dictionary.copy()
+
+    for key in multiplied_dict:
+        multiplied_dict[key] *= 2
+
+    return multiplied_dict
