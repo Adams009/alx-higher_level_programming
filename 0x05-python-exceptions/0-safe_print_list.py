bb02b0265beb10c2649dd8e4cd7aba59f1854a02
Adams009 <olaadam003@gmail.com>
2023-12-18 15:26:22 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,8 @@
 def safe_print_list(my_list=[], x=0):
     c = 0
-    for datas in my_list:
-        if c < x:
-            print(datas, end="")
-            c = c + 1
+    for datas in range(x):
+        print(datas, end="")
+        c = c + 1
 
-    except IndexERROR:
+    except IndexError:
         break
