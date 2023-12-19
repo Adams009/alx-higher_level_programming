604da104a833290c506a47f0e9f791703ca99b6c
Adams009 <olaadam003@gmail.com>
2023-12-19 14:25:55 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """
-    This is the Square class.
+    """This is the Square class.
 
-    the square class is empty, so for it not to give error we will assign pass to it for now and in the near future when we want to make use of it with attributes or methods we come back to it
+    the square class is empty, so for it not to give error we will assign pass to it for now and in the near future when we want to make use of it with attributes or methods we come for it
+
     """
+
     pass
-
-    """
-    pass was passed to it for it not to give error and for future use purpose
-
-    """
