03753e443325adf239e983bbf61ddf55be1b79ef
Adams009 <olaadam003@gmail.com>
2023-12-19 14:48:29 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,12 +2,6 @@
 """ A module for a square"""
 
 class Square:
-    """This is the Square class.
-
-    the square class is empty, so for it not to give error we will assign pass
-    to it for now and in the near future when we want to make use of it wit
-    attribute or methods we come for it
-
-    """
+    """This is the Square class. """
 
     pass
