b23c23580bc96b594a7684ad732c46fa84619fbe
Adams009 <olaadam003@gmail.com>
2023-12-19 14:59:25 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class square:
+    """This is the Square class."""
+
+    def __init__(self, size):
+        """Initializes a new instance of the Square class
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
