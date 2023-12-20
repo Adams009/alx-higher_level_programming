28187ea4c939030f7211e34fa4b3a00e78709f50
Adams009 <olaadam003@gmail.com>
2023-12-20 14:59:58 -0800

    5-raise_exception.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print("Exception caught: {}".format(e))
+    raise NameError(message)
