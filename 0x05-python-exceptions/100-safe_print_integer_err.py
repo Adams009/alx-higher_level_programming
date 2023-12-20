19f2a17d511248fa68370361a82307e493b624ed
Adams009 <olaadam003@gmail.com>
2023-12-20 15:14:59 -0800

    go

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    integer = True
+    try:
+        print("{:d}".format(value))
+    except:
+        print('Exception:', e, file=sys.stderr)
+        integer = False
+    return new
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        var = fce(*args)
+        return var
+    except Exception as e:
+        print("Exception: {}".format(e), file-str.stderr
+                return None
