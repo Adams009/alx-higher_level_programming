f398778f5d71a85cb404cf8dcbe4f48fd731b7dd
Adams009 <olaadam003@gmail.com>
2024-01-04 17:10:55 -0800

    README 0-rectangle.py 1-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ The Rectangle class """
 
+
 class Rectangle:
     """ Empty Rep """
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ The Rectangle Class """
 
+
 class Rectangle:
     """ announcing the properties in it """
     def __init__(self, width=0, height=0):
@@ -16,9 +17,9 @@ class Rectangle:
         @width.setter
         def width(self, value):
             """ retriever private instance attribute width """
-            if not isinstance(value, int):
+            if type(value) is not int:
                 raise TypeError("width must be an integer")
-            elif value < 0:
+            if value < 0:
                 raise ValueError("width must be >= 0")
             else:
                 self.__width = value
@@ -33,7 +34,7 @@ class Rectangle:
             """ retriever private instance attribute height """
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
-            elif value < 0:
+            if value < 0:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
