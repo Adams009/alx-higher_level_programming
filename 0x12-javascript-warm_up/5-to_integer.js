ceae262030e8d0433b8fb6bdc711479cf6361b8f
Adams009 <olaadam003@gmail.com>
2024-03-11 06:29:06 -0700

    good

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const first = parseInt(args[0]);
+
+if (!isNaN(first)) {
+  console.log(`My number: ${first}`);
+} else {
+  console.log('Not a number');
+}
