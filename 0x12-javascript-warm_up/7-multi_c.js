68800d00639f079361e9548c033fce94fccbe335
Adams009 <olaadam003@gmail.com>
2024-03-11 06:50:50 -0700

    good

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -8,7 +8,7 @@ let i;
 
 if (!isNaN(first)) {
   for (i = 0; i < first; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
