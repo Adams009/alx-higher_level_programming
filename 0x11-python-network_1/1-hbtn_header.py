a9d8e4ad2b9de04e8081448cce0502297d047bb6
Adams009 <olaadam003@gmail.com>
2024-03-28 21:51:04 -0700

    go

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" taking URL as argument and sending as request """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        request_id = response.headers['X-Request-Id']
+        print(f"X-Request-Id value: {request_id}")
