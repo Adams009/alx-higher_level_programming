8d7a2d13fbc3244837632d5f2aefe7259ccf3fc6
Adams009 <olaadam003@gmail.com>
2024-03-31 03:34:50 -0700

    go

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,13 @@
-"""
-Sends a request to the URL and displays the value of the variable X-Request-Id
-in the response header"""
+#!/usr/bin/python3
+"""Sends a request to the URL and displays variable X-Request-Id"""
 
 if __name__ == '__main__':
     from requests import get
     from sys import argv
+
+     if len(argv) != 2:
+         print("Usage: python3 script.py <URL>")
+         exit(1)
 
     res = get(argv[1])
     print(res.headers.get('X-Request-Id'))
